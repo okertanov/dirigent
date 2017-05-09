a152bb132d98acafc2db9392012643b4e3722a73
Oleg Kertanov <kertanov@kertanov-mbp.office.ctco.lv>
2017-05-09 18:41:07 +0300

    Remove unused UI, Add image button added

--- Xamarin/Dirigent.Net/UI/FirstViewController.designer.cs
+++ Xamarin/Dirigent.Net/UI/FirstViewController.designer.cs
@@ -1,14 +1,26 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
-namespace Dirigent.Net.UI {
-	[Register("FirstViewController")]
-	partial class FirstViewController {
-		void ReleaseDesignerOutlets() {
+namespace Dirigent.Net.UI
+{
+	[Register ("FirstViewController")]
+	partial class FirstViewController
+	{
+		[Outlet]
+		UIKit.UIButton AddImageButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (AddImageButton != null) {
+				AddImageButton.Dispose ();
+				AddImageButton = null;
+			}
 		}
 	}
 }
