5af6671c0e0acf2d0abbd04d703c748da808b3a0
Oleg Kertanov <kertanov@kertanov-mbp.office.ctco.lv>
2017-05-09 15:46:00 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,14 @@
+Dirigent - route by image
+=========================
+
+Ideas
+-----
+ - Navigate to an image's exif geodata
+ - Route by the series of the images
+ - Gearch for image if no geodata
+ - Take photo from an app
+ - Add bookmark
+ - Track the route
+ - Scan the Gallery & render on a map
+ - iCloud container for images
+ - iCloud KV Storage
---
+++ iOS/Dirigent/Dirigent/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dirigent
+//
+//  Created by Oleg Kertanov on 09/05/17.
+//  Copyright © 2017 Software Solutions SIA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
