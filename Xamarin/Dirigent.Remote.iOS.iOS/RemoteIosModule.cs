d0ae5845fefe743ae42e0910cfe2872d8d146e7c
Oleg Kertanov <okertanov@gmail.com>
2017-06-22 20:11:54 +0300

    Register storage & database

--- Xamarin/Dirigent.Remote.iOS.iOS/RemoteIosModule.cs
+++ Xamarin/Dirigent.Remote.iOS.iOS/RemoteIosModule.cs
@@ -1,15 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Dirigent.Common.Core.IoC;
 using Dirigent.Common.Core.Module;
-using UIKit;
+using Dirigent.Remote.iOS.Services.Impl;
+using Dirigent.Remote.Services;
 
 namespace Dirigent.Remote.iOS {
 	public class RemoteIosModule : IModule {
+        private readonly IIoCContainer container;
+        private readonly IMessengerHub messenger;
+
 		public RemoteIosModule(IIoCContainer container, IMessengerHub messenger) {
-			var label = new UILabel();
+			this.container = container;
+			this.messenger = messenger;
 		}
 
 		public Task Init() {
+			container.Register<IDatabaseService, FirebaseDatabaseService>();
+			container.Register<IStorageService, FirebaseStorageService>();
 			return Task.FromResult(true);
 		}
 	}
